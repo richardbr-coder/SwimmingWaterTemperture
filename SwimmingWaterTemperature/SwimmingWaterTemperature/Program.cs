e4ebc06235133aaacd02c84708de07c9c6500524
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 15:15:53 -0230

    Add files via upload

---
+++ SwimmingWaterTemperature/SwimmingWaterTemperature/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwimmingWaterTemperature
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Write("\nPlease enter the water temperature : ");
+                int x = Int32.Parse(ReadLine());
+                try
+                {
+                    if (CheckComfort(x))
+                    {
+                        Write(x + " degrees is comfortable for swimming.");
+                    }
+                    else
+                    {
+                        Write(x + " degrees is not comfortable for swimming.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    WriteLine(e);
+                }
+            }
+        }
+
+        public static Boolean CheckComfort(int temp)
+        {
+            if (temp >= 70 && temp <= 85)
+            {
+                return true;
+            }
+            else if (temp >= 32 && temp <= 212)
+            {
+                return false;
+            }
+            else
+            {
+                throw new ArgumentException("Value does not fall within the exptected range.");
+            }
+        }
+    }
+}
